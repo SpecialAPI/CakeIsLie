abac1ecdacf1b85c1ff6ff721a1739161369abf1
SpecialAPI <pavlov.andrei.d@gmail.com>
2023-03-02 19:00:49 +0300

    Initial commit

---
+++ Lua/lie.lua
@@ -0,0 +1,16 @@
+table.insert(editor_objlist_order, "text_lie")
+
+editor_objlist["text_lie"] = 
+{
+	name = "text_lie",
+	sprite_in_root = false,
+	unittype = "text",
+	tags = {"text_quality","abstract"},
+	tiling = -1,
+	type = 2,
+	layer = 20,
+	colour = {4, 0},
+	colour_active = {4, 1},
+}
+
+formatobjlist()
---
+++ README.md
@@ -0,0 +1,2 @@
+# CakeIsLie
+ A mod for Baba is You that adds a new property (LIE), a new theme and also cube sprites form new adventures.
